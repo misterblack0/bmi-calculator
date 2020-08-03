95c50a221a8741e26f95608c11d36bb5e08531b0
misterblack0 <misterblack0@protonmail.com>
2020-08-03 17:03:20 +0300

    gender functionality

--- app.js
+++ app.js
@@ -9,6 +9,36 @@ const result = document.getElementById("result");
 const bmiCategory = document.getElementById("bmi-category");
 const btn = document.querySelector(".btn");
 
+const maleBtn = document.getElementById("male");
+const femaleBtn = document.getElementById("female");
+
+let male = false;
+let female = false;
+
+maleBtn.addEventListener("click", function () {
+  male = true;
+  maleBtn.classList.toggle("gender-active");
+  document.querySelector(".male-icon").classList.toggle("icon-active");
+
+  if (female === true) {
+    female = false;
+    femaleBtn.classList.remove("gender-active");
+    document.querySelector(".female-icon").classList.remove("icon-active");
+  }
+});
+
+femaleBtn.addEventListener("click", function () {
+  female = true;
+  femaleBtn.classList.toggle("gender-active");
+  document.querySelector(".female-icon").classList.toggle("icon-active");
+
+  if (male === true) {
+    male = false;
+    maleBtn.classList.remove("gender-active");
+    document.querySelector(".male-icon").classList.remove("icon-active");
+  }
+});
+
 subtractAge.addEventListener("click", function () {
   let i = Number(age.value);
   age.value = --i;
@@ -57,16 +87,31 @@ btn.addEventListener("click", function () {
   total = weight.value / (heightInMeters * heightInMeters);
   result.textContent = total.toFixed(2);
 
-  if (total < 15) bmiCategory.textContent = "Very severely underweight";
-  else if (total >= 15 && total <= 16)
-    bmiCategory.textContent = "	Severely underweight";
-  else if (total >= 16 && total <= 18.5)
-    bmiCategory.textContent = "Underweight";
-  else if (total >= 18.5 && total <= 25) bmiCategory.textContent = "Normal";
-  else if (total >= 25 && total <= 30) bmiCategory.textContent = "Overweight";
-  else if (total >= 30 && total <= 35)
-    bmiCategory.textContent = "Moderately obese";
-  else if (total >= 35 && total <= 40)
-    bmiCategory.textContent = "Severely obese";
-  else if (total >= 40) bmiCategory.textContent = "Very severely obese";
+  if (male === true) {
+    if (total < 15) bmiCategory.textContent = "Very severely underweight";
+    else if (total >= 15 && total <= 16)
+      bmiCategory.textContent = "	Severely underweight";
+    else if (total >= 16 && total <= 18.5)
+      bmiCategory.textContent = "Underweight";
+    else if (total >= 18.5 && total <= 25) bmiCategory.textContent = "Normal";
+    else if (total >= 25 && total <= 30) bmiCategory.textContent = "Overweight";
+    else if (total >= 30 && total <= 35)
+      bmiCategory.textContent = "Moderately obese";
+    else if (total >= 35 && total <= 40)
+      bmiCategory.textContent = "Severely obese";
+    else if (total >= 40) bmiCategory.textContent = "Very severely obese";
+  } else if (female === true) {
+    if (total < 15) bmiCategory.textContent = "Very severely underweight";
+    else if (total >= 14 && total <= 15)
+      bmiCategory.textContent = "	Severely underweight";
+    else if (total >= 15 && total <= 17.5)
+      bmiCategory.textContent = "Underweight";
+    else if (total >= 17.5 && total <= 24) bmiCategory.textContent = "Normal";
+    else if (total >= 24 && total <= 30) bmiCategory.textContent = "Overweight";
+    else if (total >= 30 && total <= 35)
+      bmiCategory.textContent = "Moderately obese";
+    else if (total >= 35 && total <= 40)
+      bmiCategory.textContent = "Severely obese";
+    else if (total >= 40) bmiCategory.textContent = "Very severely obese";
+  }
 });
